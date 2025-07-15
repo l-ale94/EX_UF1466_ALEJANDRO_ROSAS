731cf393221184ca592d841968cb3fbd3de0f282
L.Ale <lrtrabajo94@gmail.com>
2025-07-15 02:00:08 +0200

    creo que va

--- EX_01/comidita.py
+++ EX_01/comidita.py
@@ -1,15 +1,30 @@
 menu = {
-    "pizza margarita": 8.99,
-    "hamburguesa clasica": 5.99,
-    "ensalada cesar": 7.5,
-    "agua mineral": 1.5
+    "Pizza Margarita": 8.99,
+    "Hamburguesa Clásica": 5.99,
+    "Ensalada César": 7.5,
+    "Agua Mineral": 1.5
 }
 
-# print(menu["Ensalada Cesar"])
-
 def place_order(selected_food, money):
-    pass
-    
-    if selected_food not in menu:
-        raise ValueError("El alimento seleccionado no está en el menu")
-    
+    try:
+        print("\nMenú disponible:")
+        for food, price in menu.items():
+            print(f"{food}: {price} €")
+
+        if selected_food not in menu:
+            raise ValueError("El alimento seleccionado no está en el menú")
+
+        price = menu[selected_food]
+        if price > money:
+            raise ValueError("No se disponen de suficientes fondos para realizar el pedido")
+
+        total_price = price
+        print(f"\n✅ Pedido realizado con éxito. Alimento seleccionado: {selected_food}, Total a pagar: {total_price} €")
+
+    except ValueError as error:
+        print(f"\n Error en el pedido: {error}")
+
+alimento = input("Introduce el alimento que deseas pedir: ")
+dinero = float(input("Introduce la cantidad de dinero que tienes: "))
+
+place_order(alimento, dinero)
